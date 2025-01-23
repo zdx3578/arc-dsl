6a52a3d66a7747d06aefeb71a6dc4b74816c2a01
zdx <179363811@qq.com>
2025-01-23 14:14:04 +0800

    for rosette 1.61

--- rkt/mainbig.rkt
+++ rkt/mainbig.rkt
@@ -92,10 +92,10 @@
   (define check-result (simple-check input-obj output-obj))
 
   (cond
-    ;; 1.1 匹配到了简单变换，直接输出结果
+    ;; 1.1 如果简单检测到变换成功，就返回 #t
     [check-result
      (displayln (string-append "Solution found => " (symbol->string check-result)))
-     ;; 可以根据 check-result 的不同，输出不同的 e 值
+     ;; 如果需要，可以打印出不同 e 的值:
      (displayln
       (string-append
        "#hash((e . "
@@ -106,30 +106,36 @@
          [(VMirror)  "3"])
        "))"))
      (displayln input-obj)
-     'sat]  ;; 函数返回值
+     ;; 直接返回 #t 表示成功
+     #t]
 
     ;; 1.2 否则进入 SMT 求解
     [else
-     ;; 如果没有提前返回，则执行后续与 SMT 相关的逻辑
      (define all-conditions
        (and
-         (>= e 0)
-         (< e 4)
-         ;; interp出来的结果必须等于 output-obj
-         (equal? (interp (translate e) input-obj) output-obj)))
+        (>= e 0)
+        (< e 4)
+        (equal? (interp (translate e) input-obj) output-obj)))
 
      (define result (solve (assert all-conditions)))
      (cond
        [(sat? result)
         (displayln "Solution found by SMT!")
         (displayln input-obj)
-        (displayln (model result))]
+        (displayln (model result))
+        ;; 表示成功
+        #t]
 
        [(unsat? result)
-        (displayln "SMT result: unsat. No solution!")]
+        (displayln "SMT result: unsat. No solution!")
+        ;; 表示失败
+        #f]
 
        [else
-        (displayln "SMT result: unknown...")])]))
+        (displayln "SMT result: unknown...")
+        ;; 也返回 #f，表示暂时无解
+        #f])]))
+
 
 
 
@@ -196,8 +202,6 @@
                 (define out-obj-set (objects-with-params output-grid out-param))
 
                 ;; 先假设此 param 可以搞定所有 out-obj
-
-
                 (let ([mid-iter 0])
                   (for ([out-obj (in-set out-obj-set)])
                   (define found-one? #f)
@@ -220,14 +224,17 @@
                 (when all-out-obj-solved?
                   (displayln (format "====> Param ~a solves all out-obj => This pair (pair #:~a) is success!"
                                      out-param pair-iter))
-                  (set! param-found? #t)))))
+                  (set! param-found? #t)))
+          )
+        )
 
           ;; 当 8 种 param 全部试完后，若 param-found? 仍是 #f, 说明该 pair 失败
           (unless param-found?
             (displayln (format "xxxx> This pair #:~a fails => no param works!"
                                pair-iter))
             ;; 只要有任何一个 pair 失败，就让“本文件不成功”
-            (set! file-all-pairs-success? #f)))
+            (set! file-all-pairs-success? #f))
+      )
     )
   )
 
